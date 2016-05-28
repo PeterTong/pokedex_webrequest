8ec87ed0a09b64520d99abbff3a252b892b2ebda
PeterTong <tkwpeter21@gmail.com>
2016-05-28 18:58:32 +0800

    first project commit

---
+++ pokedex_webrequest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex_webrequest
+//
+//  Created by KwokWing Tong on 28/5/2016.
+//  Copyright © 2016 Tong Kwok Wing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
